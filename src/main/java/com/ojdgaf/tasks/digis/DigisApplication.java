5050ab1fc6785c3a0f88936d35b6c8d41bbd7b66
Eugene Burdeinyi <ojdgaf@gmail.com>
2019-05-10 21:49:04 +0300

    init

---
+++ src/main/java/com/ojdgaf/tasks/digis/DigisApplication.java
@@ -0,0 +1,13 @@
+package com.ojdgaf.tasks.digis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigisApplication.class, args);
+	}
+
+}
