b9b6dd49758d0389b3a1cf49acfb97ef03a511af
Eugene Burdeinyi <ojdgaf@gmail.com>
2019-05-11 17:00:03 +0300

    using JUnit 5

--- src/test/java/com/ojdgaf/tasks/digis/DigisApplicationTests.java
+++ src/test/java/com/ojdgaf/tasks/digis/DigisApplicationTests.java
@@ -1,16 +1,15 @@
 package com.ojdgaf.tasks.digis;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class DigisApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {}
 
 }
